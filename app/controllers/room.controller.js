259a5aa791d4f8833f5df54b07c6af2948a225bd
rasoolabdi <M0rd00r@yahoo.com>
2025-12-21 23:13:04 -0800

    added create room & get all rooms

--- app/controllers/room.controller.js
+++ app/controllers/room.controller.js
@@ -1,6 +1,46 @@
+const createHttpError = require("http-errors");
 const Controller = require("../Controller");
+const ConversationModel = require("../models/conversation");
+const {StatusCodes: HttpStatus} = require("http-status-codes")
 
 class RoomController extends Controller {
+
+    async addNewRoom(req , res , next) {
+        try {
+            const {title , endpoint} = req.body;
+            const rooms = await ConversationModel.create({title , endpoint});
+            if(!rooms) {
+                throw createHttpError.BadRequest("محل گفتگو ایجاد نشد")
+            }
+            return res.status(HttpStatus.CREATED).json({
+                statusCode: HttpStatus.CREATED,
+                data: {
+                    message: "محل گفتکو با موفقیت ایجاد شد"
+                }
+            })
+        }
+        catch(error) {
+            next(error);
+        }
+    };
+
+    async getAllRoom(req , res , next) {
+        try {
+            const rooms = await ConversationModel.find({});
+            if(!rooms) {
+                throw createHttpError.NotFound("هیچ گفتگویی یافت نشد")
+            };
+            return res.status(HttpStatus.OK).json({
+                statusCode: HttpStatus.OK,
+                data: {
+                    rooms
+                }
+            })
+        }
+        catch(error) {
+            next(error);
+        }
+    }
 
 };
 
--- app/router/room.router.js
+++ app/router/room.router.js
@@ -1,5 +1,9 @@
 const { Router } = require("express");
+const roomController = require("../controllers/room.controller");
 const router = Router();
+
+router.post("/add" , roomController.addNewRoom);
+router.get("/list" , roomController.getAllRoom);
 
 module.exports = {
     RoomRoutesApi: router
