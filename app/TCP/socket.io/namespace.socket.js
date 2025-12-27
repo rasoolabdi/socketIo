5ef3b9a7c03c5f59aeb6401704ba0ddb4d6a5b39
rasoolabdi <M0rd00r@yahoo.com>
2025-12-26 22:20:23 -0800

    create list namespaces

--- app/TCP/socket.io/namespace.socket.js
+++ app/TCP/socket.io/namespace.socket.js
@@ -1,13 +1,25 @@
+const ConversationModel = require("../../models/conversation");
+
 class NameSpaceSocketHandler {
     #io;
     constructor(io) {
         this.#io = io;
     }
 
-    initConnection() {
-        this.#io.on("connection" , (socket) => {
-            console.log(socket.rooms);
+    async initConnection() {
+        this.#io.on("connection" , async (socket) => {
+            const namespaces = await ConversationModel.find({} , {title: 1, endpoint: 1}).sort({_id: -1});
+            socket.emit("namespacesList" , namespaces);
         })
+    }
+
+    async createNamespacesConnection() {
+        const namespaces = await ConversationModel.find({} , {title: 1, endpoint: 1, rooms: 1}).sort({_id: -1});
+        for(const namespace of namespaces) {
+            this.#io.of(`/${namespace.endpoint}`).on("connection" , (socket) => {
+                socket.emit("roomList" , namespace.rooms);
+            })
+        }
     }
 };
 module.exports = NameSpaceSocketHandler;
--- public/dist/socket.js
+++ public/dist/socket.js
@@ -1,5 +1,15 @@
 const socket = io("http://localhost:5000");
 
 socket.on("connect" , () => {
-
+    socket.on("namespacesList" , (namespacesList) => {
+        const namespacesElement = document.getElementById("namespaces");
+        namespacesElement.innerHTML = "";
+        for(const namespaces of namespacesList) {
+            const li = document.createElement("li");
+            const p = document.createElement("p");
+            p.innerText = namespaces.title;
+            li.appendChild(p);
+            namespacesElement.appendChild(li);
+        }
+    })
 })
