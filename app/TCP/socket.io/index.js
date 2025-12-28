8b7269d53e97cb3104764ec306ddad36fed7e53e
rasoolabdi <M0rd00r@yahoo.com>
2025-12-28 00:39:06 -0800

    added show groups in namespaces

--- app/TCP/socket.io/index.js
+++ app/TCP/socket.io/index.js
@@ -4,5 +4,6 @@ const NameSpaceSocketHandler = require("./namespace.socket")
 module.exports = {
     socketHandler: (io) => {
         new NameSpaceSocketHandler(io).initConnection();
+        new NameSpaceSocketHandler(io).createNamespacesConnection();
     }
 }
--- public/dist/socket.js
+++ public/dist/socket.js
@@ -1,15 +1,57 @@
 const socket = io("http://localhost:5000");
 
+function stringToHTML(str) {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(str , "text/html");
+    return doc.body.firstChild;
+}
+
+function initNamespaceConnection(endpoint){
+    const namespaceSocket = io(`http://localhost:5000/${endpoint}`);
+    namespaceSocket.on("connect" , () => {
+        namespaceSocket.on("roomList" , (rooms) => {
+            const roomElement = document.querySelector("#contacts ul");
+            roomElement.innerHTML = "";
+            for(const room of rooms) {
+                const html = stringToHTML(`
+                    <li class="contact">
+                        <div class="wrap">
+                            <div class="meta">
+                                <p class="name">${room.description}</p>
+                                <p class="preview">${room.description}</p>
+                            </div>
+                        </div>
+                    </li>
+                `)
+                roomElement.appendChild(html)
+            }
+        })
+    })
+}
+
 socket.on("connect" , () => {
     socket.on("namespacesList" , (namespacesList) => {
         const namespacesElement = document.getElementById("namespaces");
         namespacesElement.innerHTML = "";
+        initNamespaceConnection(namespacesList[3].endpoint)
         for(const namespaces of namespacesList) {
             const li = document.createElement("li");
             const p = document.createElement("p");
+            p.setAttribute("class" , "namespaceTitle");
+            p.setAttribute("endpoint" , namespaces.endpoint);
             p.innerText = namespaces.title;
             li.appendChild(p);
             namespacesElement.appendChild(li);
         }
+        
+        //click on namespace
+        const namespaceNodes = document.querySelectorAll("#namespaces li p.namespaceTitle");
+        for(const namespace of namespaceNodes) {
+            namespace.addEventListener("click" , () => {
+                const endpoint = namespace.getAttribute("endpoint");
+                initNamespaceConnection(endpoint);
+            })
+        }
     })
+
 })
